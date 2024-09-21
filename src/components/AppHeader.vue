6ffe693034ac441e3f7bcb4f06d6f83a91bcf0de
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-09-21 21:56:37 +0200

    initial structure

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,29 @@
+<script>
+import { store } from '../store.js';
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<template>
+    <nav class="navbar bg-dark mb-5">
+        <div class="container-fluid">
+            <a class="navbar-brand text-red fs-3 fw-bold">BOOLFLIX</a>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-danger" type="button">Search</button>
+            </form>
+        </div>
+    </nav>
+</template>
+
+<style lang="scss" scoped>
+ a {
+    color: #E50914;
+ }
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,27 @@
+<script>
+import { store } from '../store.js';
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<template>
+    <section class="container">
+        <div class="row row-cols-3">
+            <div class="col text-white text-center">
+                ciao
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+    div.row {
+        height: 400px;
+    }
+</style>
