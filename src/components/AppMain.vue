9b9ac0db3183158e83a581c3c3ff3d4ac2a3347c
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-09-23 17:29:19 +0200

    fixing movies posters

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,24 @@
 <script>
 import { store } from '../store.js';
+import axios from 'axios';
 
 export default {
     data() {
         return {
             store,
+            posterMoviesSrc: 'https://image.tmdb.org/t/p/w342/'
         }
     },
-    
+    methods: {
+        getPosterPic() {
+            console.log('POSTER!')
+            axios.get(this.posterMoviesSrc, {
+                params: {
+                    poster_path: store.moviesList.poster_path
+                }
+            })
+        },
+    },
 }
 </script>
 
@@ -16,10 +27,16 @@ export default {
 
         <!-- ! FILM SECTION -->
         <section>
-            <h2 v-show="store.moviesList.length !== 0" class="mb-4 text-white">FILMS</h2>
+            <h2 v-show="store.moviesList.length !== 0" class="text-white">FILMS</h2>
+            <h5 v-show="store.moviesList.length !== 0" class="mb-4 text-white">Risultati trovati: {{ store.moviesList.length }}</h5>
 
             <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
                 <div class="col text-white text-center g-5" v-for="(movie, index) in store.moviesList" :key="index">
+
+                    <div>
+                        <img :src="getPosterPic[index]" alt="movie poster">
+                    </div>
+
                     <ul class="p-0">
                         <li>
                             <b>Titolo:</b> {{ movie.title }}
@@ -55,7 +72,8 @@ export default {
 
         <!-- ! TV SERIES SECTION -->
         <section>
-            <h2 v-show="store.tvSeriesList.length !== 0" class="mb-4 text-white">SERIE TV</h2>
+            <h2 v-show="store.tvSeriesList.length !== 0" class="text-white">SERIE TV</h2>
+            <h5 v-show="store.tvSeriesList.length !== 0" class="mb-4 text-white">Risultati trovati: {{ store.tvSeriesList.length }}</h5>
 
             <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
                 <div class="col text-white text-center g-5" v-for="(serie, index) in store.tvSeriesList" :key="index">
@@ -98,6 +116,10 @@ export default {
     div.col {
         min-height: 400px;
         cursor: pointer;
+
+        img {
+            max-width: 100%;
+        }
 
         ul {
             list-style-type: none;
