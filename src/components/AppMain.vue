fcf0af7d5e941a7ab19b2b34e347032b6a1a2483
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-09-23 12:31:11 +0200

    added languages flags

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -13,7 +13,7 @@ export default {
 
 <template>
     <section class="container">
-        <div class="row row-cols-3">
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
             <div class="col text-white text-center" v-for="(movie, index) in store.moviesList" :key="index">
                 <ul>
                     <li>
@@ -22,8 +22,20 @@ export default {
                     <li>
                         <b>Titolo originale:</b> {{ movie.original_title }}
                     </li>
-                    <li>
-                        <b>Lingua originale:</b> {{ movie.original_language }}
+                    <li v-if="movie.original_language === 'en'">
+                        <b>Lingua originale:</b> <img src="../assets/img/united-kingdom.png" alt="uk_flag">
+                    </li>
+                    <li v-else-if="movie.original_language === 'it'">
+                        <b>Lingua originale:</b> <img src="../assets/img/92144_italy_icon.png" alt="it_flag">
+                    </li>
+                    <li v-else-if="movie.original_language === 'fr'">
+                        <b>Lingua originale:</b> <img src="../assets/img/92086_france_icon.png" alt="fr_flag">
+                    </li>
+                    <li v-else-if="movie.original_language === 'es'">
+                        <b>Lingua originale:</b> <img src="../assets/img/spain.png" alt="es_flag">
+                    </li>
+                    <li v-else-if="movie.original_language === 'de'">
+                        <b>Lingua originale:</b> <img src="../assets/img/92094_germany_icon.png" alt="de_flag">
                     </li>
                     <li>
                         <b>Media dei voti:</b> {{ movie.vote_average }}
@@ -35,11 +47,18 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-    div.row {
-        height: 400px;
+    div.col {
+        min-height: 400px;
+        cursor: pointer;
 
         ul {
             list-style-type: none;
+
+            img {
+                display: inline;
+                height: 25px;
+                margin-left: 10px;
+            };
         }
     }
 </style>
